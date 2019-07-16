bdd28c56bce1ab606502cf4ce26c5136fc465acc
Anurag Bhattacharya <abhatt66@my.centennialcollege.ca>
2019-07-16 11:42:32 -0400

    The Splash Page

--- Lab10Demo/Program.cs
+++ Lab10Demo/Program.cs
@@ -16,7 +16,7 @@ namespace Lab10Demo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new StartForm());
         }
     }
 }
