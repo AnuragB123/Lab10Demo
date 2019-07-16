d16c8dadaa07efb1181248ef04a6c8e2de6d994b
Anurag Bhattacharya <abhatt66@my.centennialcollege.ca>
2019-07-16 11:25:20 -0400

    Prep Part

--- Lab10Demo/Program.cs
+++ Lab10Demo/Program.cs
@@ -16,7 +16,7 @@ namespace Lab10Demo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
